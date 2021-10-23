12766d775036ddd8001fb034649080fc95ceda48
ChunYu Chang <mr91217@gmail.com>
2021-10-23 11:45:45 -0400

    add apolloserver

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 
+
+
 const path = require("path");
 const db = require("./config/connection");
 
@@ -15,14 +17,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // create a new Apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+async function startAppolloServer(){
 
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start()
+  server.applyMiddleware({ app });
+
+
+}
+
+startAppolloServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -42,9 +50,6 @@ db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-});
-db.on("error", (err) => {
-  console.error("MongoDB connection error: ", err);
 });
